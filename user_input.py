c7ba25c97ed218bcdee48df8711d9ec42df22355
Shashank <nee-github-email@gmail.com>
2026-06-08 11:36:59 +0530

    Python Task 01 Completed - Shashank ECE JNTUHUCES

---
+++ user_input.py
@@ -0,0 +1,7 @@
+name = input("Enter your name: ")
+age = input("Enter your age: ")
+tech = input("Enter your favorite technology: ")
+
+print(f"\nHello {name}!")
+print(f"You are {age} years old.")
+print(f"Your favorite technology is {tech}.")
