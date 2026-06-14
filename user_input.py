3708e68643b1833941bee29c07deb558abc9bd1e
Shashank <nee-github-email@gmail.com>
2026-06-14 17:27:11 +0530

    Removed Task 03 files from root

--- user_input.py
+++
@@ -1,7 +0,0 @@
-name = input("Enter your name: ")
-age = input("Enter your age: ")
-tech = input("Enter your favorite technology: ")
-
-print(f"\nHello {name}!")
-print(f"You are {age} years old.")
-print(f"Your favorite technology is {tech}.")
